f6bedb21c2932e80c06ff0a1f5820c0e813c8a47
Ashutosh Mishra <mishrashutosh1998@gmail.com>
2025-05-22 08:33:58 +0530

    login and register

--- notification-service/src/main/java/com/resume/notificationservice/repository/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.resume.notificationservice.repository;
-
-import com.resume.notificationservice.entity.Notification;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface NotificationRepository extends MongoRepository<Notification, String> {
-    List<Notification> findByEmployeeId(String employeeId);
-}
--- resume-service/src/main/java/com/resume/resumeservice/repository/ResumeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.resume.resumeservice.repository;
-
-import com.resume.resumeservice.entity.Resume;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface ResumeRepository extends MongoRepository<Resume, String> {
-    List<Resume> findByEmployeeId(String employeeId);
-}
